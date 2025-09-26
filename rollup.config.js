334a35917f6ae09bee9dc96c184c59b0490a92e0
Cyril <64255424+Cyril56@users.noreply.github.com>
2025-09-26 12:34:11 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
-import resolve from 'rollup-plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/ultimate-netatmo-card.ts',
   output: {
-    file: 'ultimate-netatmo-card.js',
+    file: 'dist/ultimate-netatmo-card.js',
     format: 'es'
   },
   plugins: [resolve(), typescript()]
 };
+
 
 
