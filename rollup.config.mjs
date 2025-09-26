c6cf27b5f47c9b660d3a5d280428ed3968021295
Cyril <64255424+Cyril56@users.noreply.github.com>
2025-09-26 14:34:18 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,18 +3,16 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "src/ultimate-netatmo-card.ts", // fichier d'entrée
+  input: "src/ultimate-netatmo-card.ts",
   output: {
-    file: "dist/ultimate-netatmo-card.js", // fichier généré
-    format: "es",                          // module ES
+    file: "dist/ultimate-netatmo-card.js",
+    format: "es",
     sourcemap: true
   },
   plugins: [
     resolve(),
     commonjs(),
-    typescript({
-      tsconfig: "./tsconfig.json"
-    }),
-    terser()
+    typescript({ tsconfig: "./tsconfig.json" })
+    // terser() supprimé, ne plus rien appeler ici
   ]
 };
