fd1fcebecf43343897602757291717f763fc4526
Cyril <64255424+Cyril56@users.noreply.github.com>
2025-09-26 14:30:45 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/ultimate-netatmo-card.ts", // fichier d'entrée
