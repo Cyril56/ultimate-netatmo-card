4c57576d9a9b0b81ab47972dce372e031e51d84c
Cyril <64255424+Cyril56@users.noreply.github.com>
2025-09-26 14:53:01 +0200

    Update and rename rollup.config.mjs to rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: "src/ultimate-netatmo-card.ts",
   output: {
     file: "dist/ultimate-netatmo-card.js",
     format: "es",
-    sourcemap: true
+    sourcemap: true,
+    inlineDynamicImports: true // essentiel pour générer un bundle complet
   },
   plugins: [
     resolve(),
     commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" })
-    // terser() supprimé, ne plus rien appeler ici
+    typescript({ tsconfig: "./tsconfig.json" }),
   ]
 };
